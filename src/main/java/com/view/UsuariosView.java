ca8f75f46556d70a788a3acde75df8d41021157f
gabriel.trujillo <gabrieltrujillotb@gmail.com>
2025-05-18 22:29:44 +0100

    S2 / Exportar Usuarios - Add functionality to export users to CSV file from UsuariosController and update UsuariosView

--- src/main/java/com/view/UsuariosView.java
+++ src/main/java/com/view/UsuariosView.java
@@ -88,6 +88,7 @@ public class UsuariosView extends JPanel {
                 }
             }
         }));
+        menuPanel.add(crearBoton("Exportar", "#A5571FF", e -> controller.exportarUsuarios()));
         return menuPanel;
     }
 
